e0ac6d87ef091a7788cb050705009c0e4f022649
aleacevedo <ale.acevedo@live.com.ar>
2022-05-02 07:35:28 -0300

    feat: improve backtracking

--- src/algorithms/backtracking.py
+++ src/algorithms/backtracking.py
@@ -13,25 +13,31 @@ def filter_visited(neighbors, visited):
     return res
 
 
-def solution_recursive(model, graph, next_stop_id, path, actual_charge, solutions, total_distance):
+def solution_recursive(model, graph, next_stop_id, path, actual_charge, solutions, total_distance, bestSolution):
+
     if len(path) == 151:
-        solutions.append((path, {"weight": total_distance}))
+        solutions.append(
+            (path, {"weight": total_distance, "charge": actual_charge}))
         return path
     neighbors = [(list(graph.nodes(data=True))[nei], graph.get_edge_data(
         next_stop_id, nei)) for nei in list(nx.all_neighbors(graph, next_stop_id))]
+
     neighbors = sort_by_distance(neighbors)
     posibles_stops = filter_visited(neighbors, path)
+
     for neighbor in posibles_stops:
         neighbor_id = neighbor[0][0]
-        if actual_charge + model.requests[neighbor_id] <= model.capacity and actual_charge + model.requests[neighbor_id] >= 0:
+        if actual_charge + model.requests[neighbor_id] <= model.capacity and actual_charge + model.requests[neighbor_id] >= 0 and bestSolution[1]["weight"] >= total_distance + neighbor[1]['weight']:
             actual_charge += model.requests[neighbor_id]
             total_distance += neighbor[1]['weight']
             path.append(neighbor_id)
-            temp_sol = solution_recursive(
-                model, graph, neighbor_id, path, actual_charge, solutions, total_distance)
-            if temp_sol == None:
-                path.pop()
-
+            if(len(solutions) > 0):
+                solution_recursive(
+                    model, graph, neighbor_id, path, actual_charge, solutions, total_distance, solutions[-1])
+            else:
+                solution_recursive(
+                    model, graph, neighbor_id, path, actual_charge, solutions, total_distance, bestSolution)
+            path.pop()
     return None
 
 
@@ -41,9 +47,10 @@ def solution(model):
     actual_charge = 0
     first_stop_id = 0
     solutions = []
+    bestSolution = (None, {"weight": 8639.94})  # Mi mejor solucion hasta ahora
     sol = solution_recursive(
-        model, graph, first_stop_id, path, actual_charge, solutions, 0)
+        model, graph, first_stop_id, path, actual_charge, solutions, 0, bestSolution)
     # print(sol, len(sol))
     sorted_solutions = sort_by_distance(solutions)
     print(sorted_solutions[0])
-    return sorted_solutions[0][0]
+    return sorted_solutions[0][1:]
